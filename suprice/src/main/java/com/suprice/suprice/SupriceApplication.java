0f4a7ec77dbded41d36f4a7383ccc6b7a0c4b2b6
Iustus Renidet <iustusrenidet@gmail.com>
2025-09-29 17:43:17 -0600

    inicio

---
+++ suprice/src/main/java/com/suprice/suprice/SupriceApplication.java
@@ -0,0 +1,13 @@
+package com.suprice.suprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupriceApplication.class, args);
+	}
+
+}
---
+++ suprice/src/test/java/com/suprice/suprice/SupriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprice.suprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
